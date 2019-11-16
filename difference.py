d7149c323b765896e5aabdefdb59e1be655c9f92
ganeshteja36 <57834999+ganeshteja36@users.noreply.github.com>
2019-11-16 19:18:47 +0530

    Create difference.py

---
+++ difference.py
@@ -0,0 +1,10 @@
+n=int(input("enter a number:"))
+#sum of the squares of the first n natural numbers
+a=(n*(n+1)*(2*n+1))/6
+#sum of n natural numbers
+b=(n*(n+1))/2
+#square of b
+b=b**2
+# difference between a and b
+c=b-a
+print(c)
